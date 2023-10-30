89c3ec57131b672ea1e831b8fb3cb6e75396f0bc
Devendra Patil <devendra.patil@virtoustack.com>
2023-10-30 18:36:41 +0530

    lint fix

---
+++ src/modules/book_module.ts
@@ -0,0 +1,72 @@
+import { Router } from 'express';
+import book from '../models/book';
+
+export const books = Router();
+
+// Add book record
+books.post("/addBook", async (request: any, response: any) => {
+    let bookData = await book.findOne({ bookName: request.body.bookName });
+    if (bookData) {
+        return response.status(400).send({
+            message: "Book already exist!",
+        });
+    } else {
+        if (!request.body.bookName || !request.body.authorName) {
+            return response.status(400).send({
+                message: "Required fields are missing",
+            });
+        } else {
+            const bookObj = new book({
+                bookName: request.body.bookName,
+                authorName: request.body.authorName,
+                amount: request.body.amount,
+                type: request.body.type
+            });
+            await bookObj.save();
+            response.status(200).send({
+                message: "Successfully added a Book!",
+                result: bookObj,
+            });
+        }
+    }
+});
+
+// update book record
+books.put("/updateBook/:id", async (request, response) => {
+    const id = request.params.id;
+    const body = request.body;
+    try {
+        const updatedBook = await book.findByIdAndUpdate(id, body, { new: true });
+        if (!updatedBook) {
+            return response.status(404).send({ message: "Book not found" });
+        }
+        return response.status(200).json({
+            message: "Book updated successfully!",
+            result: updatedBook,
+        });
+    } catch (error) {
+        return response.status(500).send({
+            message: "Something went wrong",
+            error: error
+        });
+    }
+});
+
+// get all books record
+books.get("/getAllBooks", async (request, response) => {
+    try {
+        const recordCount = await book.count();
+        const booksData = await book.find();
+        response.status(200).json({
+            message: "successful fetching of Book data",
+            recordCount: recordCount,
+            result: booksData,
+        });
+    } catch (error) {
+        response.status(500).json({
+            message: "Something went wrong",
+            error: error,
+        });
+    }
+});
+
